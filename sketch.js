7ce7ad93b4b1a08da25f46048a8edcc82c3f8b33
Saman Tehrani <samanrtehrani@gmail.com>
2018-03-28 21:44:05 -0400

    inint

--- sketch.js
+++ sketch.js
@@ -6,21 +6,30 @@ var ww;
 var hh;
 var ang;
 var smal_rect_width;
+var cnv;
+
+function centerCanvas() {
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+}
+
+function windowResized() {
+  centerCanvas();
+}
 
 function setup() {
 	colorMode(HSB,360,100,100,100)
-	
-	console.log(windowWidth)
-console.log(windowHeight)	
-	START_POINT = [200, 200]
 	RECT_NUM = 10;
 	ROTATION = (PI / 2) / (RECT_NUM - 1);
-	ROTATION_STEP = PI / 720;
+	ROTATION_STEP = 2 * PI / 8000;
 	STEP_COUNT = 0;
+	START_POINT = [200, 200]
 	ww = windowWidth - START_POINT[0];
 	hh = windowHeight - START_POINT[1];
-	createCanvas(windowWidth,windowHeight);
-  	background(4,59,94,100);
+	cnv = createCanvas(windowWidth,windowHeight).parent("canvas-container")
+	centerCanvas()
+	background(4,59,94,100);
 	RECT_WIDTH = min(ww, hh) / (2 * RECT_NUM - 1);
 
 }
@@ -40,10 +49,10 @@ function draw() {
 			translate(x, y);
 			rotate(ROTATION*i + ROTATION*j + ROTATION_STEP * STEP_COUNT);
 			fill(0, 0, 100);
-			rect(0, 0, RECT_WIDTH, RECT_WIDTH); 
+			rect(0, 0, RECT_WIDTH, RECT_WIDTH);
 			pop();
 		}
 	}
 	pop()
-	STEP_COUNT = STEP_COUNT >= 360 ? 0 : STEP_COUNT +1;
+	STEP_COUNT = STEP_COUNT >= 8000 ? 0 : STEP_COUNT +1;
   }
